8758822115e9a0b7129b51ce49bd52f0018c71b6
AlexUrbaez13 <aj.urbaez@gmail.com>
2025-04-08 18:50:34 -0700

    a

--- components/GenerationsContainer.tsx
+++ components/GenerationsContainer.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+/* eslint-disable  @typescript-eslint/no-explicit-any */
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import pokemonGenerations from '@/data/pokemonGenerations';
