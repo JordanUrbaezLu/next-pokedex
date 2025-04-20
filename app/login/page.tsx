68fa53b2c3e5f63274a1e181c831961e22473419
AlexUrbaez13 <aj.urbaez@gmail.com>
2025-04-20 07:49:54 -0700

    :s

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,18 +7,6 @@ const LoginPage = () => {
    
     className="bg-blue-300/40 p-6 rounded-md shadow-md w-100 h-90 text-center"
   >
-  <img
-    src="/pokeball/pokeball.png"
-    alt="Pokéball"
-    className="w-10 h-10 mx-auto mb-4 border rounded-md ml-1 mt-1"
-  />
-  
-  <img
-    src="/pokemonlogo.png"
-    alt="Pokemon Logo"
-    className="w-30 h-30 mx-auto mb-4 -mt-26"
-  />
-  
     <h1 className="text-xl font-bold mb-2 ">Next Pokedex</h1>
     <h2 className="text-lg font-bold mb-4">Login</h2>
     <input
