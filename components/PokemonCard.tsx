417f782b8a3886cbe77286de9b00fb985c2bddab
AlexUrbaez13 <aj.urbaez@gmail.com>
2025-05-04 10:12:24 -0700

    a

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -31,64 +31,64 @@ const PokemonCard = ({
   }, [pokemon]);
 
   return (
-    <div
-      className="flex flex-col items-center justify-center 
+    <div className="ml-4 translate-x-2">
+      <div
+        className="flex flex-col items-center justify-center 
                  w-[120px] md:w-[200px] 
                  max-h-[230px] md:max-h-[265px] 
                  p-1 md:p-4 
                  m-1 md:m-2 
-                 rounded-md 
-                 border-2 md:border-3 border-sky-500 
-                 bg-sky-200 
+                 bg-white
                  cursor-pointer 
-                 hover:bg-sky-300 active:bg-sky-500 
+                 hover:bg-gray-300 active:bg-gray-500 
                  shadow-md hover:shadow-lg 
                  transition-colors duration-200 ease-out 
                  text-xs md:text-sm font-bold pokemon-font"
-      onClick={handleClick}
-    >
-      {loading && (
-        <div className="p-2 md:p-[20px]">
-          <RingLoader
-            color="#ffffff"
-            loading={loading}
-            size={80} // smaller spinner for mobile
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
+        onClick={handleClick}
+      >
+        {loading && (
+          <div className="p-2 md:p-[20px]">
+            <RingLoader
+              color="#ffffff"
+              loading={loading}
+              size={80} // smaller spinner for mobile
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
+        )}
+
+        <img
+          className="max-h-[120px] min-h-[120px] max-w-[120px] min-w-[120px] md:max-h-[168px] md:min-h-[168px] md:max-w-[168px] md:min-w-[168px]"
+          src={isShiny ? pokemon?.shinyImg : pokemon?.img}
+          width={120}
+          height={120}
+          alt={`${pokemon.name} image`}
+          onLoad={() => setLoading(false)}
+          style={{ display: loading ? 'none' : 'block' }}
+        />
+
+        <div className="mt-1 md:mt-2">{pokemon.id}</div>
+
+        <div className="flex gap-[6px] md:gap-[10px] px-[2px] md:px-[5px] mt-1">
+          {pokemon?.type1 && (
+            <img
+              className="aspect-[4/1] w-[50px] md:w-[70px]"
+              src={pokemonTypes[pokemon.type1]}
+              alt={`${pokemon.type1} type image`}
+            />
+          )}
+          {pokemon?.type2 && (
+            <img
+              className="aspect-[4/1] w-[50px] md:w-[70px]"
+              src={pokemonTypes[pokemon.type2]}
+              alt={`${pokemon.type2} type image`}
+            />
+          )}
         </div>
-      )}
 
-      <img
-        className="max-h-[120px] min-h-[120px] max-w-[120px] min-w-[120px] md:max-h-[168px] md:min-h-[168px] md:max-w-[168px] md:min-w-[168px]"
-        src={isShiny ? pokemon?.shinyImg : pokemon?.img}
-        width={120}
-        height={120}
-        alt={`${pokemon.name} image`}
-        onLoad={() => setLoading(false)}
-        style={{ display: loading ? 'none' : 'block' }}
-      />
-
-      <div className="mt-1 md:mt-2">{pokemon.id}</div>
-
-      <div className="flex gap-[6px] md:gap-[10px] px-[2px] md:px-[5px] mt-1">
-        {pokemon?.type1 && (
-          <img
-            className="aspect-[4/1] w-[50px] md:w-[70px]"
-            src={pokemonTypes[pokemon.type1]}
-            alt={`${pokemon.type1} type image`}
-          />
-        )}
-        {pokemon?.type2 && (
-          <img
-            className="aspect-[4/1] w-[50px] md:w-[70px]"
-            src={pokemonTypes[pokemon.type2]}
-            alt={`${pokemon.type2} type image`}
-          />
-        )}
+        <div className="text-center mt-1">{pokemon.name}</div>
       </div>
-
-      <div className="text-center mt-1">{pokemon.name}</div>
     </div>
   );
 };
