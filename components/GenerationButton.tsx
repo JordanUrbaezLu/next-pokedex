303f675e52cbe4a21784f4898502516baa7b1341
AlexUrbaez13 <aj.urbaez@gmail.com>
2025-05-08 09:14:15 -0700

    feat: changed style on GenerationButton component

--- components/GenerationButton.tsx
+++ components/GenerationButton.tsx
@@ -11,7 +11,7 @@ const GenerationButton = ({ generation }: { generation: any }) => {
   return (
     <div className="flex pl-6 ml-4 translate-x-30">
       <Link
-        className="p-1 mt-2 mb-3 bg-gray-200 cursor-pointer hover:bg-gray-300 active:bg-gray-500 shadow-md transition-colors duration-200 ease-out hover:shadow-lg text-xs font-bold"
+        className="p-1 mt-2 mb-2 bg-gray-200 cursor-pointer hover:bg-gray-300 active:bg-gray-500 shadow-md transition-colors duration-200 ease-out hover:shadow-lg text-xs font-bold"
         href={generation.href}
       >
         {generation.name}
