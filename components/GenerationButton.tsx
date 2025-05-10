a1f7918e73dcdcf4b4fa747a165eb53e8c6d2efb
AlexUrbaez13 <aj.urbaez@gmail.com>
2025-05-09 18:47:35 -0700

    style: created proper spacing between generation buttons

--- components/GenerationButton.tsx
+++ components/GenerationButton.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 
 const GenerationButton = ({ generation }: { generation: any }) => {
   return (
-    <div className="flex pl-8 ml-10 translate-x-30">
+    <div className="flex pl-2 ml-10 translate-x-30">
       <Link
         className="p-1 mt-2 mb-3 bg-gray-200 cursor-pointer hover:bg-gray-300 active:bg-gray-500 shadow-md transition-colors duration-200 ease-out hover:shadow-lg text-xs font-bold"
         href={generation.href}
