979cf9e666388c77b52df3843f3bd8ea926a75a3
AlexUrbaez13 <aj.urbaez@gmail.com>
2025-05-11 16:43:57 -0700

    feat: changed gen text/styles and made stats bar color appear for stats 150+ and fixed test for the gen text change

--- components/GenerationButton.tsx
+++ components/GenerationButton.tsx
@@ -11,7 +11,7 @@ const GenerationButton = ({ generation }: { generation: any }) => {
   return (
     <div className="flex ml-3 translate-x-95">
       <Link
-        className="translate-x-95 p-2 mb-3 rounded-full bg-gray-200 cursor-pointer hover:bg-gray-300 active:bg-gray-500 shadow-md transition-colors duration-200 ease-out hover:shadow-lg text-xs font-bold"
+        className="translate-x-95 p-2 mb-2 rounded-full bg-gray-200 cursor-pointer hover:bg-gray-300 active:bg-gray-500 shadow-md transition-colors duration-200 ease-out hover:shadow-lg text-xs font-bold"
         href={generation.href}
       >
         {generation.name}
