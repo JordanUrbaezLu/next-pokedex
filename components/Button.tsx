b2a5721a6e0de6c25ba55f45dab8a2f3e57af86e
AlexUrbaez13 <aj.urbaez@gmail.com>
2025-05-24 19:38:43 -0700

    feat: removed import line from navigation.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -31,8 +31,7 @@ const Button = ({
     </Link>
   ) : (
     <button
-    className="inline-block p-2 px-5 py-2.5 m-2 text-sm font-medium text-center text-white bg-red-700 rounded-full hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-    
+      className="inline-block p-2 px-5 py-2.5 m-2 text-sm font-medium text-center text-white bg-red-700 rounded-full hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
       onClick={onClick}
     >
       {name}
